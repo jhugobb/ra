dc9325bbdc7afb401933f1c6b1ab48cf3e99c966
jhugobb <hugo.b.brandao@gmail.com>
2019-10-25 10:50:05 +0200

    Add code that works with 16 bits

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <chrono>
 #include <bitset>
+#include <algorithm>
 
 using namespace std;
 
@@ -43,7 +44,7 @@ bool operator<(const std::bitset<N> &x, const std::bitset<N> &y)
 int main(int argc, char **argv)
 {
 
-    const size_t N_debug = 500;
+    const size_t N_debug = 8;
 
     std::multiset<bitset<N_debug>*, classcomp> S_set;
 
@@ -52,45 +53,68 @@ int main(int argc, char **argv)
     mt19937 mersenne_generator(rd()); // mt19937 is a standard mersenne_twister_engine
 
     uniform_int_distribution<size_t> uni(0, 1); // guaranteed unbiased
+
     int curr;
     bitset<N_debug> *s_member;
-    for (unsigned int i = 0; i < 5000; i++)
-    {
-        s_member = new bitset<N_debug>;
-        for (unsigned int j = 0; j < N_debug; j++)
-        {
-            curr = uni(mersenne_generator);
-            s_member->set(j, curr);
-        }
-        S_set.insert(s_member);
-    }
+    int n_ones = 0;
+    std::vector<int> results;
+    for (unsigned int k = 0; k < 1000; k++) {
+      
+      S_set.clear();
+      bool i_chose_one = false;
+      
+      for (unsigned int i = 0; i < 5000; i++)
+      {
+          s_member = new bitset<N_debug>;
+          for (unsigned int j = 0; j < N_debug; j++)
+          {
+              curr = uni(mersenne_generator);
+              s_member->set(j, curr);
+          }
+          S_set.insert(s_member);
+          if(s_member->all()){
+            i_chose_one = true;
+            n_ones++;
+            break;
+          } 
+      }
 
-    unsigned int count = 1;
-    auto end = std::prev(S_set.end());
+      unsigned int count = 1;
+      if (!i_chose_one) {
+        auto end = std::prev(S_set.end());
 
-    bitset<N_debug>* a;
-    bitset<N_debug>* b;
-    bool is_dominated;
-    for (auto it_i=S_set.begin(); it_i!=end; ++it_i) {
-        a = *it_i;
-        is_dominated = false;
-        for (auto it_j=std::next(it_i); it_j!=S_set.end(); ++it_j) {
-            b = *it_j;
-            if (IsDominated(*a,*b)) {
-                is_dominated = true;
-                break;
+        bitset<N_debug>* a;
+        bitset<N_debug>* b;
+        bool is_dominated;
+        for (auto it_i=S_set.begin(); it_i!=end; ++it_i) {
+            a = *it_i;
+            is_dominated = false;
+            for (auto it_j=std::next(it_i); it_j!=S_set.end(); ++it_j) {
+                b = *it_j;
+                if (IsDominated(*a,*b)) {
+                    is_dominated = true;
+                    break;
+                }
+            }
+            if (!is_dominated) {
+                count++;
             }
         }
-        if (!is_dominated) {
-            count++;
-        }
+      }
+      // cout << "Max value on the set: " << *(*--S_set.end()) << endl;
+      // cout << "Number of dominant bitsets: " << count <<  << \irflush;
+      cout << k << '\r' << flush;
+
+      // cout << (*min_s < *max_s) << endl;
+      results.push_back(count);
     }
+    
+    std::sort(results.begin(), results.end());
 
-    cout << "Min value on the set: " << *(*S_set.begin()) << endl;
-    cout << "Max value on the set: " << *(*--S_set.end()) << endl;
-    cout << "Number of dominant bitsets: " << count << endl;
+    cout << "Median of the experiment: ";
+    cout << results[results.size()/2] << endl;
 
-    // cout << (*min_s < *max_s) << endl;
+    cout << "Number of times we chose 1's: " << n_ones << endl;
 
     return 0;
 }
